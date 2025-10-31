1c802bce3a2133903192f4343b302dc5bbf9c6e7
Serebro1 <OtcheskovSemen@yandex.ru>
2025-11-01 00:36:14 +0300

    update mpi task

--- tasks/otcheskov_s_elem_vec_avg/mpi/src/ops_mpi.cpp
+++ tasks/otcheskov_s_elem_vec_avg/mpi/src/ops_mpi.cpp
@@ -34,8 +34,8 @@ bool OtcheskovSElemVecAvgMPI::RunImpl() {
   int local_size = total_size / ProcNum;
   int remainder = total_size % ProcNum;
 
-  int my_size = local_size + (ProcRank < remainder ? 1 : 0);
-  std::vector<int> local_data(my_size);
+  int proc_size = local_size + (ProcRank < remainder ? 1 : 0);
+  std::vector<int> local_data(proc_size);
 
   std::vector<int> displacements(ProcNum);
   std::vector<int> counts(ProcNum);
@@ -45,25 +45,15 @@ bool OtcheskovSElemVecAvgMPI::RunImpl() {
     counts[i] = local_size + (i < remainder ? 1 : 0);
     displacements[i] = offset;
     offset += counts[i];
+    
   }
-
   MPI_Scatterv(GetInput().data(), counts.data(), displacements.data(), MPI_INT,
-               local_data.data(), my_size, MPI_INT, 0, MPI_COMM_WORLD);
-
+               local_data.data(), proc_size, MPI_INT, 0, MPI_COMM_WORLD);
   int local_sum = std::accumulate(local_data.begin(), local_data.end(), 0);
 
-  int global_sum = 0;
-  MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-
-  double average = 0;
-  if (ProcRank == 0) {
-    average = global_sum / static_cast<double>(total_size);
-  }
-
-  MPI_Bcast(&average, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-  GetOutput() = average;
-
+  int total_sum = 0;
+  MPI_Allreduce(&local_sum, &total_sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  GetOutput() =  total_sum / static_cast<double>(total_size);
   return true;
 }
 
